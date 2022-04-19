4d4d489268f04bb3f7461f16c23443d4e868de55
ishajazmine <a.fredericksen@live.com.au>
2022-04-19 20:36:13 +0800

    19/04/2022 - Django Review
    19/04/2022 - Django Project Review

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,31 @@
 from django.urls import path
 
 from . import views
+
+# urlpatterns = [
+#     # polls
+#     path('', views.index, name='index'),
+#     # ex: /polls/34/
+#     path('specifics/<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/34/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/34/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+# OLD 29/01/22
+# app_name = 'polls'
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
